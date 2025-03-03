ca1fab172afbfa1a5066927c7713997bbd637688
Triplelight <dev@triplelight.dev>
2025-03-03 03:01:24 +0200

    ig that's all

---
+++ script.js
@@ -0,0 +1,20 @@
+// Scroll Animations
+const sections = document.querySelectorAll('.fade-in');
+
+const options = {
+    root: null,
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-in');
+            observer.unobserve(entry.target);
+        }
+    });
+}, options);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
